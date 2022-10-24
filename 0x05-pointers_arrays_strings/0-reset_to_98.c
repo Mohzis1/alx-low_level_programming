e68e94cb3faf5eb511690c86570e21bf60eafdb1
Mohzis1 <isangmoses0@gmail.com>
2022-10-24 08:45:05 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - returns value of n to 98
+ * @n:  value to be printed
+ * Return: n
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - This will be used for this task
+ *
+ * Return: 0
+ */
+int _putchar(int n)
+{
+	return (write(1, &c, 1));
+}
