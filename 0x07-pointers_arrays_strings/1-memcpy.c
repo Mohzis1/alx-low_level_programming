d6544cc871ecefd76bdf4ce7a93a909b67244778
Mohzis1 <isangmoses0@gmail.com>
2022-11-02 05:52:51 -0700

    kkk

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,13 +6,14 @@
  * @src: memory to be copied
  * @n: number of bytes
  * Return: pointer to @dest
+ *
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
-	for (a = 0; a < n; i++)
+	for (a = 0; a < n; a++)
 	{
 		dest[a] = src[a];
 	}
