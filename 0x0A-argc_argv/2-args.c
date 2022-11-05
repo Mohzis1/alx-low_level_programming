0a5d85f35079a6f7c9994efb86db4fb937286fc0
Mohzis1 <isangmoses0@gmail.com>
2022-11-05 05:02:08 -0700

    frfr

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the name of the program
+ * @argc: count argument
+ * @argv: argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	if (argc > 0)
+	{
+		while (count < 0)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
