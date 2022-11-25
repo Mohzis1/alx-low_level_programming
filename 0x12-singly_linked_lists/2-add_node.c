23879ed575b68ecff33b3b9ecc30da226c263591
Mohzis1 <isangmoses0@gmail.com>
2022-11-24 19:08:30 -0800

    klklkl

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,26 +1,44 @@
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include "lists.h"
-
 /**
- * add_node - add a new node at beginning of a list_t list.
- * @head: head of a list_t list.
- * @str: value to insert into element.
- * Return: the number of nodes.
+ * add_node - Adds a new node
+ * @head: The original linked list
+ * @str: The string to add to the node
+ * Return: address of the new list or NULL if it failed
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *add;
+list_t *temp;
+if (head != NULL && str != NULL)
+{
+temp = malloc(sizeof(list_t));
+if (temp == NULL)
+return (NULL);
+temp->str = strdup(str);
+temp->len = _strlen(str);
+temp->next = *head;
+*head = temp;
+return (temp);
+}
+return (0);
+}
 
-	add = malloc(sizeof(list_t));
 
-	if (add == NULL)
-	return (NULL);
-	add->str = strdup(str);
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ * Return: String length
+ */
+int _strlen(const char *s)
+{
+	int c = 0;
 
-		add->len = _strlen(str);
-		add->next = *head;
-		*head = add;
-		return (add);
+	while (*s)
+	{
+	s++;
+	c++;
+	}
+	return (c);
 }
