17897b822875bb2dd347420177e1035f2b6390e4
Mohzis1 <isangmoses0@gmail.com>
2022-11-24 19:12:50 -0800

    pppoo

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,19 +10,22 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *temp;
-if (head != NULL && str != NULL)
-{
-temp = malloc(sizeof(list_t));
-if (temp == NULL)
-return (NULL);
-temp->str = strdup(str);
-temp->len = _strlen(str);
-temp->next = *head;
-*head = temp;
-return (temp);
-}
-return (0);
+	list_t *temp;
+
+	if (head != NULL && str != NULL)
+	{
+		temp = malloc(sizeof(list_t));
+
+		if (temp == NULL)
+			return (NULL);
+
+		temp->str = strdup(str);
+		temp->len = _strlen(str);
+		temp->next = *head;
+		*head = temp;
+		return (temp);
+	}
+	return (0);
 }
 
 
